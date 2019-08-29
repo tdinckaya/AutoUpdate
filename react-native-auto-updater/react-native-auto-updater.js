1bf409452feec3a68ad3025b541373435a9e9963
Tansel Dinçkaya <tansel@101evler.com>
2019-08-29 17:00:15 +0300

    .

---
+++ react-native-auto-updater/react-native-auto-updater.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Rahul Jiresal on 01/22/16.
+ */
+
+'use strict';
+
+var React = require('react-native');
+var RNAUNative = React.NativeModules.ReactNativeAutoUpdater;
+
+type Props = {
+  isVisible: boolean;
+}
+
+module.exports = {
+  jsCodeVersion: function() {
+  	return RNAUNative.jsCodeVersion;
+  },
+  path: function() {
+  	return RNAUNative.path;
+  }
+};
